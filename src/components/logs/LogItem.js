b8e8939078312dd0998688ad9e43d7a6c98a88b6
Brian <brian.holland.pr@gmail.com>
2020-05-26 22:25:13 -0700

    added Momentjs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,11 +1,25 @@
 import React from "react";
+import Moment from "react-moment";
 import PropTypes from "prop-types";
 
 const LogItem = ({ log }) => {
     return (
         <li className="collection-item">
             <div>
-                <a href="">{log.message}</a>
+                <a
+                    href="#edit-log-modal"
+                    className={`modal-trigger ${
+                        log.attention ? "red-text" : "blue-text"
+                    }`}
+                >
+                    {log.message}
+                </a>
+                <br />
+                <span className="grey-text">
+                    <span className="black-text">ID#{log.id} </span>Last updated
+                    by: <span className="black-text">{log.tech}</span> on{" "}
+                    <Moment format="MMMM do YYYY, h:mm:ss a">{log.date}</Moment>
+                </span>
             </div>
         </li>
     );
